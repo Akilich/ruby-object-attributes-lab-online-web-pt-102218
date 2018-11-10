70cf82048a844073d1c2f9e02e7865bc629fbdf1
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-10 20:21:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,2 @@
 class Dog
-  def name=(name)
-    @name= "Fido"
-end
-def name 
-  name = "Fido"
-  end
-
-def breed=(breed)
-  @breed= "Beagle"
-  end
-def breed
-  breed = "Beagle"
-  end
-end
-
-def name=(name)
+  
